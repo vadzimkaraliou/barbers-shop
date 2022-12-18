45a138ccab551c38c7d5a7a5c45510cd323a85df
vadzimkaraliou <vadim.koroleov@gmail.com>
2022-12-18 19:53:01 +0100

    Updates

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,52 +1,71 @@
-import React, {useContext, useState} from 'react';
-import {Context} from "../../context/AppContext";
+import React, { useContext, useState } from "react";
+import { Context } from "../../context/AppContext";
 import ShoppingCart from "../ShoppingCart/ShoppingCart";
-import {MdOutlineShoppingCart} from 'react-icons/md';
-import {RiSearchLine} from 'react-icons/ri';
-import {Link} from "react-router-dom";
-import { useTranslation, Trans } from 'react-i18next';
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { RiSearchLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import { useTranslation, Trans } from "react-i18next";
 
 const lngs = {
-    pl: { nativeName: 'PL' },
-    en: { nativeName: 'EN' }
-  };
+  pl: { nativeName: "PL" },
+  en: { nativeName: "EN" },
+};
 
 const Navigation = () => {
-    const { state } = useContext(Context);
-    const [isShoppingCartVisible, setIsShoppingCartVisible] = useState(false);
+  const { state } = useContext(Context);
+  const [isShoppingCartVisible, setIsShoppingCartVisible] = useState(false);
 
-    const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
-    const handleToggleShoppingCart = () => {
-        setIsShoppingCartVisible(!isShoppingCartVisible);
-    }
+  const handleToggleShoppingCart = () => {
+    setIsShoppingCartVisible(!isShoppingCartVisible);
+  };
 
-    return (
-        <nav className="nav">
-            <Link to="/"><img src={require("../../assets/KB.png")} alt="logo" className="navLogo"
-                              title={t('goHome')}/></Link>
+  return (
+    <nav className="nav">
+      <Link to="/">
+        <img
+          src={require("../../assets/KB.png")}
+          alt="logo"
+          className="navLogo"
+          title={t("goHome")}
+        />
+      </Link>
 
-            <div className="nav-icons">
-                <Link to="/SEARCH"><RiSearchLine className="navBasket" title={t('searchProducts')}/></Link>
+      <div className="nav-icons">
+        <Link to="/SEARCH">
+          <RiSearchLine className="navBasket" title={t("searchProducts")} />
+        </Link>
 
-                {state.addedItems.length > 0 ? <div className="quantity"> { state.addedItems.map(item => item.quantity).reduce((total, item) => total + item) } </div> : null }
-                <MdOutlineShoppingCart className="navBasket" title={t('showCart')}
-                                       onClick={handleToggleShoppingCart}/>
+        {state.addedItems.length > 0 ? (
+          <div className="quantity">
+            {" "}
+            {state.addedItems
+              .map((item) => item.quantity)
+              .reduce((total, item) => total + item)}{" "}
+          </div>
+        ) : null}
+        <MdOutlineShoppingCart
+          className="navBasket"
+          title={t("showCart")}
+          onClick={handleToggleShoppingCart}
+        />
 
-                {isShoppingCartVisible ? <ShoppingCart/> : null}
-              
-                {Object.keys(lngs).map((lng) => (
-                <button key={lng} className="languageBtn" type="submit" onClick={() => i18n.changeLanguage(lng)}>
-                {lngs[lng].nativeName}
-                </button>
-          ))}
+        {isShoppingCartVisible ? <ShoppingCart /> : null}
 
-
-            </div>
-        </nav>
-    )
-
-}
+        {Object.keys(lngs).map((lng) => (
+          <button
+            key={lng}
+            className="languageBtn"
+            type="submit"
+            onClick={() => i18n.changeLanguage(lng)}
+          >
+            {lngs[lng].nativeName}
+          </button>
+        ))}
+      </div>
+    </nav>
+  );
+};
 
 export default Navigation;
-
